97a12ed4f98265273a8ddef0090ce1321c0a35f9
stas <stas37951@gmail.com>
2023-01-28 16:53:02 +0200

    celery done

--- store/storage/filters.py
+++ store/storage/filters.py
@@ -4,7 +4,7 @@ from .models import Order
 
 
 class ProductFilter(filters.FilterSet):
-
-    class Meta:
-        model = Order
-        fields = {'order_id_in_shop': '__in'}
+    pass
+    # class Meta:
+    #     model = Order
+    #     fields = {'order_id_in_shop': '__in'}
